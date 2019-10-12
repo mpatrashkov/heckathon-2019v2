d61849ed98ae18d1b9790236968e87779d73460b
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2019-10-12 03:09:10 +0300

    map

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import Map from "../components/here-maps/Map";
+import { geolocated } from "react-geolocated";
 
 class Maps extends Component {
     render() {
         return (
             <div>
-                Maps
+                {this.props.coords ? (
+                    <Map lat={this.props.coords.latitude} lon={this.props.coords.longitude} zoom={11}/>
+                ) : (
+                     <div>Getting the location data&hellip; </div>
+                )}
             </div>
         );
     }
 }
 
-export default Maps;
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+})(Maps);
