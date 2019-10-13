b821af1396fb66b4bc46ef17f2dfb02a46d99a7f
Georgi Atanasov <36049559+nigosto@users.noreply.github.com>
2019-10-13 06:47:36 +0300

    added handle submit

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        123
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            nodeId: '',
+            fish: false,
+            temp: 0
+        }
+    }
+
+    handleSubmit = async () => {
+        e.preventDefault()
+
+        let data = await fetch("http://192.168.0.179:9999/node/create/record", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+
+        let result = await data.json()
+        if(!result.nodeRecord) {
+            return null;
+        } else {
+            return result.nodeRecord;
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        );
+    }
 }
 
 export default App;
